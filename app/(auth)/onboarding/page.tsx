d6df896682e0bd61395eea6b83822add6767b7f5
eugenetangkj <dragoneugene10@gmail.com>
2024-01-06 22:52:07 +0800

    Add onboarding form layout

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,35 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
+
+//Onboarding page
 async function Page() {
+    //Get current user that is signed in
+    const user = await currentUser();
+
+    const userInfo = {};
+
+    //Get user information
+    const userData = {
+        id: user?.id,
+        objectId: userInfo?.id, //Object id of the user as stored in the database
+        username: userInfo?.username || user?.username, //Can either get from Clerk or from database
+        name: userInfo?.name || user?.firstName || "", //May be empty if dont have username
+        bio: userInfo?.bio || "",
+        image: userInfo?.image || user.imageUrl,
+    }
+
+
     return(
-        <main>
-            <h1 className='head-text'>Onboarding</h1>    
+        <main className='mx-auto flex flex-col max-w-3xl justify-start px-10 py-20'>
+            <h1 className='head-text'>Onboarding</h1> 
+            <p className='mt-3 text-base-regular text-light-2'>Complete your profile now to use Threads</p>   
+        
+            <section className='mt-9 bg-dark-2 p-10'>
+                <AccountProfile user={ userData } btnTitle="Continue" />
+
+
+            </section>
+        
         </main>
     )
 }
---
+++ lib/validations/user.ts
@@ -0,0 +1,10 @@
+import * as z from 'zod';
+
+//Rules for validating user information when filling up the form
+export const UserValidation = z.object({
+    profile_photo: z.string().url().min(1), //Ensures that string is not empty,
+    name: z.string().min(3, { message: 'Minimum 3 characters'}).max(30, { message : 'Maximum 30 characters'}),
+    username: z.string().min(3, { message: 'Minimum 3 characters'}).max(30, { message : 'Maximum 30 characters'}), 
+    bio: z.string().min(3, { message: 'Minimum 3 characters'}).max(30, { message : 'Maximum 1000 characters'}),  
+    
+});
