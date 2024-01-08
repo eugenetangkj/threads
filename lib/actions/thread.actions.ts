baf8fadea05162c3e142d708046d92b1d29a4166
eugenetangkj <dragoneugene10@gmail.com>
2024-01-08 16:57:03 +0800

    Add thread card

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -12,7 +12,7 @@ interface Params {
     path: string,
 }
 
-
+//Create a thread
 export async function createThread({text, author, communityId, path,}: Params) {
 
     try {
@@ -38,5 +38,44 @@ export async function createThread({text, author, communityId, path,}: Params) {
     }
 }
 
+
+//Fetch all posts
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    try {
+        //Connect to database
+        connectToDB();
+
+        //Calculate the number of posts to skip depending on which page we are on
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        //Find posts that have no parents (top-level threads) as we do not want
+        //to fetch posts that are comments. Show latest threads at the top.
+        const postsQuery = Thread.find({ parentId: { $in: [null, undefined]}})
+                                 .sort({ createdAt: 'desc'})
+                                 .skip(skipAmount)
+                                 .limit(pageSize)
+                                 .populate({path: 'author', model: User})
+                                 .populate({ path: 'children', populate: {
+                                    path: 'author',
+                                    model: User,
+                                    select: '_id name parentId image'
+                                 }});
+        
+        //Only want to count top level post
+        const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined ]}});
+        
+        //Execute the query
+        const posts = await postsQuery.exec();
+
+        //Check whether there are more posts that come after this page
+        const isNext = totalPostsCount > skipAmount + posts.length;
+
+        return { posts, isNext };
+
+        
+    } catch (error : any) {
+        console.log(`Cannot fetch posts: ${error.message}`);
+    }
+}
 
 
