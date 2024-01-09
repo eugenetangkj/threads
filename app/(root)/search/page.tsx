46e2e805356dd58fd8130f9f2646237b8ef34345
eugenetangkj <dragoneugene10@gmail.com>
2024-01-09 21:15:15 +0800

    Add tab pages

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,10 @@
+//Search page
+const Page = async () => {
+    return (
+        <section>
+            <h1 className='head-text mb-10'>Search</h1>
+        </section>
+    )
+}
+
+export default Page;
