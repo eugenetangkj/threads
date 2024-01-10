d3ffd9260886785abb0b94d9e5854cbb52f8b9c1
eugenetangkj <dragoneugene10@gmail.com>
2024-01-10 16:50:05 +0800

    Fix bugs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,30 @@
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
 import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
  
 //Home page that displays all the given threads
 export default async function Home() {
   const result = await fetchPosts(1, 30);
+  
+  //Get current user to know who is going to create the thread
   const user = await currentUser();
+
+  //No user is logged in. Should not allow creation of thread, so return null
+  if (! user) {
+      return null;
+  }
+
+  //Get user information
+  const userInfo = await fetchUser(user.id);
+
+  if (! userInfo?.onboarded) {
+      //User has not onboarded. Redirect to onboarding page and do not allow
+      //creation of threads
+      redirect('/onboarding');
+  }
+
 
   return (
     <>
--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -5,6 +5,6 @@ export const UserValidation = z.object({
     profile_photo: z.string().url().min(1), //Ensures that string is not empty,
     name: z.string().min(3, { message: 'Minimum 3 characters'}).max(30, { message : 'Maximum 30 characters'}),
     username: z.string().min(3, { message: 'Minimum 3 characters'}).max(30, { message : 'Maximum 30 characters'}), 
-    bio: z.string().min(3, { message: 'Minimum 3 characters'}).max(30, { message : 'Maximum 1000 characters'}),  
+    bio: z.string().min(3, { message: 'Minimum 3 characters'}).max(1000, { message : 'Maximum 1000 characters'}),  
     
 });
